905fb2820b725323531f0b8395d45831a642ca28
berkson <berksonx@yahoo.com.br>
2022-02-08 20:15:42 -0300

    testes corrigidos

--- src/app/contract/components/listing/listing.component.spec.ts
+++ src/app/contract/components/listing/listing.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -8,17 +9,19 @@ describe('ListingComponent', () => {
   let component: ListingComponent;
   let fixture: ComponentFixture<ListingComponent>;
   let router: Router;
+  let httpClient: HttpClient;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ListingComponent],
-      imports: [RouterTestingModule.withRoutes([])],
+      imports: [RouterTestingModule.withRoutes([]), HttpClientModule],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ListingComponent);
     router = TestBed.inject(Router);
+    httpClient = TestBed.inject(HttpClient);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/shared/services/http-util.service.spec.ts
+++ src/app/shared/services/http-util.service.spec.ts
@@ -1,13 +1,22 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { Router, RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HttpUtilService } from './http-util.service';
 
-describe('HttpUtilServiceService', () => {
+describe('HttpUtilService', () => {
   let service: HttpUtilService;
+  let httpClient: HttpClient;
+  let router: Router;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule.withRoutes([])],
+    });
     service = TestBed.inject(HttpUtilService);
+    httpClient = TestBed.inject(HttpClient);
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
