46d00f6a9bf78888de07ce19eb6cd7726024c549
berkson <berksonx@yahoo.com.br>
2022-05-03 20:38:38 -0300

    testes ok

--- src/app/contract/components/contract.component.spec.ts
+++ src/app/contract/components/contract.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContractComponent } from './contract.component';
@@ -8,7 +9,8 @@ describe('ContractComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContractComponent ]
+      declarations: [ ContractComponent ],
+      imports:[HttpClientModule]
     })
     .compileComponents();
   });
--- src/app/contract/components/dialogs/aditive/additive-dialog.component.spec.ts
+++ src/app/contract/components/dialogs/aditive/additive-dialog.component.spec.ts
@@ -1,6 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AditiveDialogComponent } from './additive-dialog.component';
 
@@ -11,7 +14,7 @@ describe('AditiveDialogComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AditiveDialogComponent],
-      imports:[ReactiveFormsModule],
+      imports:[ReactiveFormsModule, HttpClientModule, RouterTestingModule.withRoutes([]), MatSnackBarModule],
       providers: [
         { provide: MatDialogRef, useValue: {} },
         { provide: MAT_DIALOG_DATA, useValue: {} },
